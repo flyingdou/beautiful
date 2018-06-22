04c39095cb1d1912ea1d3c66ddef4ea54023da6d
flyingdou <1576883859@qq.com>
2018-06-22 16:18:38 +0800

    sd

--- app.js
+++ app.js
@@ -3,12 +3,12 @@ App({
   onLaunch: function () {
     // 初始化
     wx.setStorageSync('memberId', null);
-    wx.setStorageSync('clubId', 290);
+    wx.setStorageSync('clubId', 433);
   },
    constant: {
     base_pic_url: 'https://www.ecartoon.com.cn/picture',
     base_img_url: 'https://www.ecartoon.com.cn/miniProgram/club/img/'
   },
-  request_url: 'https://www.ecartoon.com.cn/clubmp!'
+  request_url: 'https://www.ecartoon.com.cn/beautifulclubmp!'
   
 })
